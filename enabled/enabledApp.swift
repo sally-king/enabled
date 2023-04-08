e86626e6660c3d7f463266fca10c8e88734694f1
sally-king <52473628+sally-king@users.noreply.github.com>
2023-04-08 10:06:47 -0700

    Initial Commit

---
+++ enabled/enabledApp.swift
@@ -0,0 +1,17 @@
+//
+//  enabledApp.swift
+//  enabled
+//
+//  Created by Sally King on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct enabledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
